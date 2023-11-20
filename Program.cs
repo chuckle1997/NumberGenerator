ee6784a4915afaf00f30bcd0f7621ff69e558d72
Charles Thomas Turner <mixerkyro@seznam.cz>
2023-11-20 10:41:36 +0100

    Generator implemented

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Metrics;
+
+int userInput1 = 20;
+int userInput2 = 25;
+int userInput3 = 5;
+
+for (int i = userInput1; i < userInput2; i++)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = userInput3; i < userInput1; i += userInput3)
+{
+    Console.WriteLine(i);
+}
+
+string textInput = Console.ReadLine();
+int textInput2 = 6;
+
+for (int i = 0; i < textInput2; i++)
+{
+    Console.WriteLine($"{i}. {textInput}");
+}
+
+
